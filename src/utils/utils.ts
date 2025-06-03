618d5507ee80e4aa6d9f887ff7d0ff92fb773208
Jiří Patejdl <jiri.patejdl@rcs-kladno.net>
2025-06-03 12:41:23 +0200

    Animace a oprava chyb

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,3 +5,8 @@ export const trim = (str = "", ch?: string) => {
   while (end > start && str[end - 1] === ch) --end;
   return start > 0 || end < str.length ? str.substring(start, end) : str;
 };
+
+
+export const animationClass = " opacity-0 intersect:opacity-100 transition-opacity intersect-once intersect-half duration-1000"
+
+export const animationClassInstant = " opacity-0 intersect:opacity-100 transition-opacity intersect-once duration-1000"
